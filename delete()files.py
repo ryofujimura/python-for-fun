b7274fc1ac0ea9b401848a1ef8b218a309622f5c
Ryo Fuj <ryo176999@gmail.com>
2022-01-24 23:12:24 -0800

    delete pictures with same file name with (#)

---
+++ delete()files.py
@@ -0,0 +1,35 @@
+# importing the library
+import os
+import sys
+import glob
+
+# # giving directory name
+dirname = '/Volumes/OREnoPIC/2015-2018/'
+newdir = '/Volumes/OREnoPIC/2015-2018/deletedfiles/'
+# # giving file extension
+
+ext = ('m4v', 'jpg','mp4')
+# # iterating over all files
+contain = ")"
+
+list = []
+
+for file in glob.glob(dirname+"*"):
+    for newfile in glob.glob(file+"/*"):
+        list.append(newfile)
+        for newnewfile in glob.glob(newfile+"/*"):
+            list.append(newnewfile)
+            for newnewnewfile in glob.glob(newnewfile+"/*"):
+                list.append(newnewnewfile)
+                for newnewnewnewfile in glob.glob(newnewnewfile+"/*"):
+                    list.append(newnewnewnewfile)
+print(list)
+for files in list:
+    # print(files)
+    if contain in files:
+        # print(files)# printing file name of desired extension
+        os.remove(files)
+    else:
+        continue
+
+    
